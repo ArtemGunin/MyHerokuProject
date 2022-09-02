e4a6f74706f410c1a5e15e9a44d00fb7e50a1abe
Artem.Gunin <artemgunin1993@gmail.com>
2022-09-02 22:20:49 +0300

    DB

--- src/main/java/com/heroku/Main.java
+++ src/main/java/com/heroku/Main.java
@@ -2,6 +2,7 @@ package com.heroku;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Main {
@@ -15,5 +16,20 @@ public class Main {
         System.out.println("Start program!");
         final Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
         System.out.println(connection.getCatalog());
+        System.out.println("Creating table in given database...");
+        String sql = "CREATE TABLE REGISTRATION " +
+                "(id VARCHAR(255), " +
+                " title VARCHAR(255), " +
+                " model VARCHAR(255), " +
+                " count INTEGER, " +
+                " PRIMARY KEY ( id ))";
+
+        try(final PreparedStatement statement = connection.prepareStatement(sql)) {
+            if (statement.execute()) {
+                System.out.println("Table created!");
+            } else {
+                System.out.println("Table not created!");
+            }
+        }
     }
 }
