c4d3c7b1a7ce9eaea6d8aca6ff40f7560365ca46
Artem.Gunin <artemgunin1993@gmail.com>
2022-09-02 23:26:09 +0300

    DB

--- src/main/java/com/heroku/Main.java
+++ src/main/java/com/heroku/Main.java
@@ -10,10 +10,11 @@ public class Main {
     private static final String PASSWORD = "65ee99abcc12509b2574314949cc3ccca0b16e821c4f9a84d11e39d8d3fae253";
 
     public static void main(String[] args) throws SQLException {
+        String id = "123";
         System.out.println("Start program!");
         final Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
         System.out.println("Creating table in given database...");
-        String sql = "CREATE TABLE my_phone1 " +
+        String sql = "CREATE TABLE new_phone" +
                 "(id VARCHAR(255), " +
                 " title VARCHAR(255), " +
                 " model VARCHAR(255), " +
@@ -24,13 +25,15 @@ public class Main {
         try (final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.executeUpdate();
             System.out.println("Table created!");
+            System.out.println("Insert values: \nid = 123\ntitle = Title - 1\nmodel = Model - 1\ncount = 3");
             save(connection);
-            printById("123", connection);
+            System.out.println("\nGet values:");
+            printById(id, connection);
         }
     }
 
     private static void save(Connection connection) {
-        String sql = "INSERT INTO \"my_phone1\" (id, title, model, count) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO \"new_phone\" (id, title, model, count) VALUES (?, ?, ?, ?)";
         try (final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, "123");
             statement.setString(2, "Title - 1");
@@ -43,16 +46,16 @@ public class Main {
     }
 
     private static void printById(String id, Connection connection) {
-        String sql = "SELECT * FROM \"my_phone1\" WHERE id = ?";
+        String sql = "SELECT * FROM \"new_phone\" WHERE id = ?";
 
         try (final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, id);
             final ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("id"));
-                System.out.println(resultSet.getString("title"));
-                System.out.println(resultSet.getString("model"));
-                System.out.println(resultSet.getInt("count"));
+                System.out.println("id = " + resultSet.getString("id"));
+                System.out.println("title = " + resultSet.getString("title"));
+                System.out.println("model = " + resultSet.getString("model"));
+                System.out.println("count = " + resultSet.getInt("count"));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
