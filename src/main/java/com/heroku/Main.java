d8b3ae258f052f6ff5c226213ea5dd6ab1fbc6aa
Artem.Gunin <artemgunin1993@gmail.com>
2022-09-02 23:42:32 +0300

    DB

--- src/main/java/com/heroku/Main.java
+++ src/main/java/com/heroku/Main.java
@@ -4,7 +4,6 @@ import java.sql.*;
 
 public class Main {
 
-    private static final String JDBC_DRIVER = "org.postgresql.Driver";
     private static final String URL = "jdbc:postgresql://ec2-44-207-126-176.compute-1.amazonaws.com:5432/dfonar3j80br1r";
     private static final String USER = "yqwmwlxfngrdxs";
     private static final String PASSWORD = "65ee99abcc12509b2574314949cc3ccca0b16e821c4f9a84d11e39d8d3fae253";
@@ -14,7 +13,7 @@ public class Main {
         System.out.println("Start program!");
         final Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
         System.out.println("Creating table in given database...");
-        String sql = "CREATE TABLE new_phone" +
+        String sql = "CREATE TABLE new_phone_table1" +
                 "(id VARCHAR(255), " +
                 " title VARCHAR(255), " +
                 " model VARCHAR(255), " +
@@ -33,7 +32,7 @@ public class Main {
     }
 
     private static void save(Connection connection) {
-        String sql = "INSERT INTO \"new_phone\" (id, title, model, count) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO \"new_phone_table1\" (id, title, model, count) VALUES (?, ?, ?, ?)";
         try (final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, "123");
             statement.setString(2, "Title - 1");
@@ -46,7 +45,7 @@ public class Main {
     }
 
     private static void printById(String id, Connection connection) {
-        String sql = "SELECT * FROM \"new_phone\" WHERE id = ?";
+        String sql = "SELECT * FROM \"new_phone_table1\" WHERE id = ?";
 
         try (final PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, id);
@@ -62,3 +61,21 @@ public class Main {
         }
     }
 }
+
+//COMMANDS
+
+//    heroku login
+//
+//    heroku git:remote -a limitless-atoll-52560
+//
+//        git commit -am "DB"
+//
+//        git push heroku master
+//
+//        heroku run bash
+//
+//        cd target/
+//
+//        java -jar HerokuProject-1.0-SNAPSHOT.jar
+//
+//        exit
